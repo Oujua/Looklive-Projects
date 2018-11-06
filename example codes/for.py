29be5f7362fabac43aeab4a33f3f545621610d73
Oujua <Mattiece.oujua@gmail.com>
2018-11-05 22:08:16 -0500

    Clear for unneeded files
    Clear

--- example codes/for.py
+++
@@ -1,6 +0,0 @@
-# for x in range(1,10):
-# 	print(x)
-# 	print(x*x)
-
-for letter in "coffee":
-	print(letter*10)
--- example codes/input.py
+++
@@ -1,6 +0,0 @@
-data = input("What's your favorite color?")
-print("You said " + data)
-
-print("What's your favorite color?")
-data = input()
-print("You said " + data)
