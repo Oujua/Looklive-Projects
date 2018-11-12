b68fbac364abd359d623f2063c80eb8be13777bc
Oujua <Mattiece.oujua@gmail.com>
2018-11-12 17:50:34 -0500

    Clean up
    Cleanup of code

--- DeckOCards.py
+++ DeckOCards.py
@@ -1,49 +1,77 @@
 from random import shuffle
+
+
 class Card:
-    def __init__(self,suit,value): #Takes in a suit and a value for each card
+    def __init__(self, suit, value):  # Takes in a suit and a value for each card
         self.suit = suit
         self.value = value
+
     def __repr__(self):
         return f"{self.value} of {self.suit}"
+
+
 class Deck:
     def __init__(self):
         self.cards = []
-        suitlist = ["Hearts", "Diamonds", "Clubs", "Spades"] #Definition of the different suits
-        valuelist = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"] #Definition of the different values
+        # Definition of the different suits
+        suitlist = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        valuelist = [
+            "Ace",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "Jack",
+            "Queen",
+            "King"]  # Definition of the different values
         for suit in suitlist:
             for value in valuelist:
-                self.cards.append(Card(suit,value)) #Assigns each card a suit
+                # Assigns each card a suit
+                self.cards.append(Card(suit, value))
         print(self.cards)
 
     def count(self):
-        return len(self.cards) #Returns the number of cards left in the deck
+        return len(self.cards)  # Returns the number of cards left in the deck
 
     def __repr__(self):
         return f"\nThere are {self.count()} cards left in the deck"
 
     def shuffle(self):
         if self.count() < 52:
-            raise ValueError(f"There must be 52 cards in the deck for a shuffle to occur! Current cards: {self.count()}") #Doesn't shuffle if less than 52 cards
-        shuffle(self.cards) #shuffles using random import (Udemy course reference used)
+            # Doesn't shuffle if less than 52 cards
+            raise ValueError(
+                f"There must be 52 cards in the deck for a shuffle to occur! Current cards: {self.count()}")
+        # shuffles using random import (Udemy course reference used)
+        shuffle(self.cards)
         return self
 
     def _deal(self, numOfCards):
         count = self.count()
         if count == 0:
-            raise ValueError("All the cards have been dealt currently.") #If deck is empty, return a different error
+            # If deck is empty, return a different error
+            raise ValueError("All the cards have been dealt currently.")
         amountDealt = min([count, numOfCards])
-        print(f"\nRemoving {amountDealt} card(s)...\n") #Removes the top card(s)
+        # Removes the top card(s)
+        print(f"\nRemoving {amountDealt} card(s)...\n")
         cards = self.cards[-amountDealt:]
         self.cards = self.cards[:-amountDealt]
         return cards
 
-    def deal_hand(self,num):
-        return self._deal(num) #Deals num amount of cards
+    def deal_hand(self, num):
+        return self._deal(num)  # Deals num amount of cards
 
     def deal_one(self):
-        return self._deal(1)[0] #Deals a single card located at the zero index, with the 1 being defined inside
+        # Deals a single card located at the zero index, with the 1 being
+        # defined inside
+        return self._deal(1)[0]
 
  ###UNIT TESTS###
+
 
 a = Deck()
 a.shuffle()
